486771fcf7eaab1072e9a611dbdb5a8722eafb46
Jarrod Mishima <mishimajam@gmail.com>
2022-11-22 10:14:29 -0800

    fixed mockdraft form

--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -1,2 +1,5 @@
 from .login_form import LoginForm
 from .signup_form import SignUpForm
+from .league_form import LeagueForm
+from .team_form import TeamForm
+from .mockdraft_form import MockDraftForm
--- app/forms/mockdraft_form.py
+++ app/forms/mockdraft_form.py
@@ -3,6 +3,6 @@ from wtforms import StringField, IntegerField, TextAreaField
 from wtforms.validators import DataRequired
 from app.models import User
 
-class LeagueForm(FlaskForm):
+class MockDraftForm(FlaskForm):
     name = StringField('name', validators=[DataRequired()])
     draft_pick = IntegerField('draft_pick', validators=[DataRequired()])
