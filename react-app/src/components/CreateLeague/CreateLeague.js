111426816c8b390cdc05e1be8526f92725d1f072
Jarrod Mishima <mishimajam@gmail.com>
2022-11-24 21:07:33 -0800

    starting create league form css

--- react-app/src/components/CreateLeague/CreateLeague.js
+++ react-app/src/components/CreateLeague/CreateLeague.js
@@ -5,21 +5,44 @@ import { useSelector } from 'react-redux';
 import './CreateLeague.css';
 import logo from '../../assets/halfcourtLogo-PhotoRoom.png'
 import NavBar from '../NavBar/NavBar';
+import Footer from '../Footer/Footer';
 
 function CreateLeague() {
   const [errors, setErrors] = useState([]);
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [leagueName, setLeagueName] = useState('')
+  const [teamName, setTeamName] = useState('')
+  const [teamLogo, setTeamLogo] = useState('')
+  const [leagueType, setLeagueType] = useState('')
+  const [draftType, setDraftType] = useState('')
+  const [leagueDesc, setLeagueDesc] = useState('')
+
+
   const user = useSelector(state => state.session.user);
   // const dispatch = useDispatch();
 
-  const updateEmail = (e) => {
-    setEmail(e.target.value);
+  const updateLeagueName = (e) => {
+    setLeagueName(e.target.value);
   };
 
-  const updatePassword = (e) => {
-    setPassword(e.target.value);
+  const updateTeamName = (e) => {
+    setTeamName(e.target.value);
   };
+  const updateTeamLogo = (e) => {
+    setTeamLogo(e.target.value);
+  };
+
+  const updateLeagueType = (e) => {
+    setLeagueType(e.target.value);
+  };
+
+  const updateDraftType = (e) => {
+    setDraftType(e.target.value);
+  };
+
+  const updateLeagueDesc = (e) => {
+    setLeagueDesc(e.target.value);
+  };
+
 
   const onSubmit = async (e) => {
 
@@ -36,32 +59,77 @@ function CreateLeague() {
             <div key={ind}>{error}</div>
           ))}
         </div>
-        <div className='login-form-email'>
-          <label className='login-form-label' htmlFor='email'>Email</label>
-          <input
-            className='login-form-input'
-            name='email'
-            type='text'
-            placeholder='Email'
-            value={email}
-            onChange={updateEmail}
-          />
+        <div className="create-league-form-league-name">
+          <label className='create-league-form-label' htmlFor='email'>League Name</label>
+            <input
+              className='create-league-form-input'
+              name='leagueName'
+              type='text'
+              placeholder='League Name'
+              value={leagueName}
+              onChange={updateLeagueName}
+            />
         </div>
-        <div className='login-form-password'>
-          <label className='login-form-label' htmlFor='password'>Password</label>
-          <input
-          className='login-form-input'
-            name='password'
-            type='password'
-            placeholder='Password'
-            value={password}
-            onChange={updatePassword}
-          />
+        <div className='create-league-form-team-name'>
+        <label className='create-league-form-label' htmlFor='email'>League Name</label>
+            <input
+              className='create-league-form-input'
+              name='leagueName'
+              type='text'
+              placeholder='League Name'
+              value={leagueName}
+              onChange={updateLeagueName}
+            />
         </div>
-        <div className='login-form-submit-holder'>
-          <button className='login-form-submit' type='submit'>LOG IN</button>
+        <div className='create-league-form-team-logo'>
+        <label className='create-league-form-label' htmlFor='email'>League Name</label>
+            <input
+              className='create-league-form-input'
+              name='leagueName'
+              type='text'
+              placeholder='League Name'
+              value={leagueName}
+              onChange={updateLeagueName}
+            />
+        </div>
+        <div className='create-league-form-league-type'>
+        <label className='create-league-form-label' htmlFor='email'>League Name</label>
+            <input
+              className='create-league-form-input'
+              name='leagueName'
+              type='text'
+              placeholder='League Name'
+              value={leagueName}
+              onChange={updateLeagueName}
+            />
+        </div>
+        <div className='create-league-form-draft-type'>
+        <label className='create-league-form-label' htmlFor='email'>League Name</label>
+            <input
+              className='create-league-form-input'
+              name='leagueName'
+              type='text'
+              placeholder='League Name'
+              value={leagueName}
+              onChange={updateLeagueName}
+            />
+        </div>
+        <div className='create-league-form-league-desc'>
+        <label className='create-league-form-label' htmlFor='email'>League Name</label>
+            <input
+              className='create-league-form-input'
+              name='leagueName'
+              type='text'
+              placeholder='League Name'
+              value={leagueName}
+              onChange={updateLeagueName}
+            />
+        </div>
+        <div className='create-league-form-submit-holder'>
+          <button className='create-league-form-submit' type='submit'>LOG IN</button>
         </div>
       </form>
+      <Footer/>
     </div>
 
   )
